ef65740b24019232a55304f3d375466af41360f7
Nulifier <nulifier@gmail.com>
2016-04-18 15:04:03 -0600

    Combined with Details clicker

--- sub-refresh.user.js
+++ sub-refresh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sub List Refresh
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @updateURL    https://github.com/Nulifier/Subbing/raw/master/sub-refresh.user.js
 // @match        https://epsb.eschoolsolutions.com/substituteAvailableJobAction.do
 // @match        https://epsb.eschoolsolutions.com/substituteAvailableJobInitAction.do
@@ -11,10 +11,22 @@
 
 (function() {
     'use strict';
+    
+    // Check if there is a job available on the page and click it
+    var jobs = $("a:contains('Details')");
+    if (jobs.length > 0) {
+        // Redirect to page
+        window.location.replace(jobs.attr('href'));
+    }
 
+    // Refresh the page after 15ish seconds
     window.setTimeout(function() {
+        // Set the date to be from today to June 30th, 2016
+        /// @todo Change this to be 30 days from today.
         $('#startDate').val(moment().format('MM/DD/YYYY'));
         $('#endDate').val('06/30/2016');
+        
+        // Search, these are the same steps the page takes
         disableSearch();
         blockUIForSearch("jobSearchToken", "jobSearchCookie");
         document.forms[0].submit();
